3d84a117ecc68cc8d1454c01ba09131ea4a3b26d
tanvir <aliashraftanvir410@gmail.com>
2025-11-26 19:57:49 +0600

    nodemon installed, server.js changed

--- server.js
+++ server.js
@@ -8,15 +8,19 @@ app.set("view engine", "ejs");
 app.get('/', (req, res) => {
     res.render('home', {});
 });
+app.get('/home', (req, res) => {
+    res.render('home', {});
+});
 
 app.get('/marketplace', (req, res) => {
-    res.render('marketplace'); 
+    res.render('marketPlace',{}); 
 });
 
 app.get('/tutorial', (req, res) => {
-    res.render('tutorial'); 
+    res.render('tutorials',{}); 
 });
 
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
 });
+// run this by : npm start
