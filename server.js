70e41ef7b22363b2461a0bc85321f9402e6102a0
tanvir <aliashraftanvir410@gmail.com>
2025-11-27 12:28:30 +0600

    User login added

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.post('/signUp', async(req, res) => {
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user
+
     const newUser = new User({
       name,
       email,
@@ -61,6 +61,53 @@ app.post('/signUp', async(req, res) => {
     res.status(500).send('Server Error');
   }
 })
+
+// login 
+app.get('/logIn', (req, res) => {
+    res.render("logIn", {});
+})
+app.post('/logIn',  async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // console.log(req.body);
+    //  Check if user exists
+    const user = await  User.findOne({ email });;
+    if (!user) {
+      return res.status(401).render("messagePage", {
+       message: 'Invalid email or password',
+     redirectUrl: "/login"
+     });
+    }
+
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+       return res.status(401).render("messagePage", {
+       message: 'Invalid password',
+       redirectUrl: "/login"
+       });
+    }
+
+
+    const token = jwt.sign({ userId: user._id, name: user.name }, jwtSecret, { expiresIn: '1h' });
+
+ 
+    res.cookie('token', token, {
+      httpOnly: true,         //  for Preventing JS access
+      maxAge: 3600000, 
+      sameSite: 'lax', 
+    });
+
+    f=1;
+    res.redirect('/home'); 
+
+  } catch (err) {
+    console.error('Login error:', err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 
 
 app.listen(port, () => {
